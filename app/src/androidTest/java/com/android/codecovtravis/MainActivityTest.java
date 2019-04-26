4ceedcc280e80e70032e0232fd2999de59a97a14
MimoSaha <mmimosaha@gmail.com>
2019-04-26 13:01:09 +0600

    Update process

--- app/src/androidTest/java/com/android/codecovtravis/MainActivityTest.java
+++ app/src/androidTest/java/com/android/codecovtravis/MainActivityTest.java
@@ -1,8 +1,6 @@
 package com.android.codecovtravis;
 
 
-import android.os.Handler;
-import android.os.Looper;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
@@ -19,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -32,30 +31,33 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    private Handler handler;
-
     @Test
     public void mainActivityTest() {
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-
-        handler = new Handler(Looper.getMainLooper());
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                ViewInteraction textView = onView(
-                        allOf(withId(R.id.hello_world_text), withText("Hello World!"),
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.start), withText("Strat"),
+                        childAtPosition(
                                 childAtPosition(
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0),
+                                        withId(android.R.id.content),
                                         0),
-                                isDisplayed()));
-                textView.check(matches(withText("Hello World!")));
-            }
-        }, 7000);
+                                0),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.status), withText("success"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("success")));
     }
 
     private static Matcher<View> childAtPosition(
